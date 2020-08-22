fd0a363a4b3a86247a15fc8eeb2878b40ccc1dd9
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-08-21 23:32:56 -0300

    Implantado JAX-WS-RS

---
+++ src/main/java/br/com/agendamento/AgendamentoApplication.java
@@ -0,0 +1,6 @@
+package br.com.agendamento;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class AgendamentoApplication {}
---
+++ src/main/java/br/com/agendamento/controller/AgendamentoEmailController.java
@@ -0,0 +1,35 @@
+package br.com.agendamento.controller;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.agendamento.model.AgendamentoEmail;
+import br.com.agendamento.service.AgendamentoEmailService;
+
+@Path("/emails")
+public class AgendamentoEmailController
+{
+	@Inject
+	private AgendamentoEmailService agendamentoEmailService;
+	
+	@GET
+	@Produces(value = MediaType.APPLICATION_JSON)
+	public Response listar()
+	{
+		return Response.ok(agendamentoEmailService.listar()).build();
+	}
+	
+	@POST
+	@Consumes(value = MediaType.APPLICATION_JSON)
+	public Response inserir(AgendamentoEmail agendamentoEmail)
+	{
+		agendamentoEmailService.inserir(agendamentoEmail);
+		return Response.status(201).build();
+	}
+}
